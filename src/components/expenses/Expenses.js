631f3a3ac5f5843b8b7837f2cd4b3f3177f89b81
Amit Kumar <koundal.amit007@gmail.com>
2022-07-27 21:50:48 +0530

    How to display data conditionally

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -6,16 +6,39 @@ import Card from "../globals/Card";
 import Filters from "./Filters";
 
 const Expenses = (props) => {
-    const [expsenseFilter, setExpsenseFilter] = useState('2020');
+    const [filterDate, setfilterDate] = useState('2020');
     const applyFilterHandler = (filter) =>{
-        // console.log(filter);
-        setExpsenseFilter(filter);
+        console.log(filter);
+        setfilterDate(filter);
     }
 
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filterDate
+    })
+
+    /**
+     * Display Content dynamically 
+     * 
+     * First Approach
+     * { filteredExpenses.length === 0 ? 'No Expense Found' : filteredExpenses.map(expense => <ExpenseItem key={expense.key} title={expense.title} price={expense.price} date={expense.date} />) } 
+     * 
+     * Second Approach
+     * { filteredExpenses.length === 0 && 'No Expense Found' }
+     * { filteredExpenses.length > 0 && filteredExpenses.length === 0 ? 'No Expense Found' : filteredExpenses.map(expense => <ExpenseItem key={expense.key} title={expense.title} price={expense.price} date={expense.date} />) }
+     * 
+     * Third Approach
+     */
+    let expenseItemeContent = 'No Expense Item found';
+
+    if(filteredExpenses.length > 0){
+        expenseItemeContent = filteredExpenses.map(expense => <ExpenseItem title={expense.title} price={expense.price} date={expense.date} key={expense.key} />)
+    }
+
+    
     return (
         <Card className="expenses">
-            <Filters selected={expsenseFilter} onFilterChnage={applyFilterHandler} />
-            { props.expenses.map(expense => <ExpenseItem key={expense.key} title={expense.title} price={expense.price} date={expense.date} />)}
+            <Filters selected={filterDate} onFilterChnage={applyFilterHandler} />
+            {expenseItemeContent}
         </Card>
         
     );
